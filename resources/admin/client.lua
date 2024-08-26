318636650b49fda437afd5fe6eabd7af941bea76
FranzKriegeld0345 <FranzKriegeld0345@users.noreply.github.com>
2024-08-26 15:54:44 +0200

    fix 0.1

--- resources/admin/client.lua
+++ resources/admin/client.lua
@@ -7,20 +7,46 @@ local targetPlayer = nil
 
 RegisterNetEvent('admin:openMenu')
 AddEventHandler('admin:openMenu', function()
-    -- NUI fókusz beállítása
+    -- Megnyitjuk a NUI felületet és beállítjuk a fókuszt
     SetNuiFocus(true, true)
-
-    -- Üzenet küldése a NUI-nak (admin_menu.html)
     SendNUIMessage({
         action = "openMenu"
     })
 end)
 
--- NUI Callback a menü bezárásához
 RegisterNUICallback('closeMenu', function(data, cb)
+    -- Bezárjuk a NUI felületet és visszaállítjuk a fókuszt
     SetNuiFocus(false, false)
     cb('ok')
 end)
+
+RegisterNetEvent('admin:spawnVehicle')
+AddEventHandler('admin:spawnVehicle', function(vehicleName)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+
+    -- Ellenőrizzük, hogy a jármű neve meg van-e adva
+    if vehicleName then
+        RequestModel(vehicleName)
+
+        -- Várjuk meg, amíg a modell betöltődik
+        while not HasModelLoaded(vehicleName) do
+            Citizen.Wait(0)
+        end
+
+        -- Hozzuk létre a járművet a játékban
+        local vehicle = CreateVehicle(vehicleName, coords.x, coords.y, coords.z, GetEntityHeading(playerPed), true, false)
+
+        -- Ha szükséges, adjuk hozzá a játékoshoz
+        SetPedIntoVehicle(playerPed, vehicle, -1)
+    else
+        -- Küldjünk vissza egy hibát, ha nincs járműnév megadva
+        TriggerEvent('chat:addMessage', {
+            args = {"^1SYSTEM", "Vehicle name is required!"}
+        })
+    end
+end)
+
 
 RegisterNetEvent('admin:spectate')
 AddEventHandler('admin:spectate', function(target)
