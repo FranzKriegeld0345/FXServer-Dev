fa5c44dbf8d566d9e3bb16f9e878b05d7e90c769
FranzKriegeld0345 <FranzKriegeld0345@users.noreply.github.com>
2024-08-26 15:07:18 +0200

    Added DevMode

---
+++ resources/fk_devmode/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- fxmanifest.lua
+
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'FK-Development'
+description 'Developer mode script for testing'
+version '1.0.0'
+
+client_script 'client.lua'
+
+dependency '/assetpacks'
